58bc62952cfb50ef0260bc4a067ab50ee6eacc13
Leonardo Almeida Araújo <leonardoxsystems@gmail.com>
2016-05-17 15:30:53 -0300

    Providers

--- src/CodeTags/Providers/CodeTagsServiceProvider.php
+++ src/CodeTags/Providers/CodeTagsServiceProvider.php
@@ -12,7 +12,7 @@ class CodeTagsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([__DIR__ . '/../../resources/migrations'=> base_path('database/migrations')], 'migrations');
-        require __DIR__ . '/../routes.php';
+        require __DIR__ . '/../../routes.php';
     }
 
     public function register()
