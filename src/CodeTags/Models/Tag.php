ea25fd0a89ee91df8d529a91c32c112a094d5852
Leonardo Almeida Araújo <leonardoxsystems@gmail.com>
2016-05-20 18:19:32 -0300

    Tests

--- src/CodeTags/Models/Tag.php
+++ src/CodeTags/Models/Tag.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: imac
- * Date: 5/16/16
- * Time: 17:33
- */
 
 namespace Leoalmar\CodeTags\Models;
 
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
@@ -21,6 +16,34 @@ class Tag extends Model
         'taggable_id',
         'taggable_type'
     ];
+
+    private $validator;
+
+    public function setValidator(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function getValidator()
+    {
+        return $this->validator;
+    }
+
+    public function isValid()
+    {
+        $validator = $this->validator;
+
+        $validator->setRules(['name'=>'required|max:255']);
+        $validator->setData( $this->attributes );
+
+        if($validator->fails()){
+            $this->errors = $validator->errors();
+            return false;
+        }
+
+        return true;
+    }
+
 
     public function taggable()
     {
--- tests/CodeTags/Models/TagTest.php
+++ tests/CodeTags/Models/TagTest.php
@@ -3,8 +3,10 @@
 namespace Leoalmar\CodeTags\Tests\Models;
 
 
+use Illuminate\Validation\Validator;
 use Leoalmar\CodeTags\Models\Tag;
 use Leoalmar\CodeTags\Tests\AbstractTestCase;
+use Mockery as M;
 
 class TagTest extends AbstractTestCase
 {
@@ -12,10 +14,53 @@ class TagTest extends AbstractTestCase
     public function setUp()
     {
         parent::setUp();
-
         $this->migrate();
     }
 
+    public function test_inject_validator_in_tag_model()
+    {
+        $tag = new Tag();
+        $validator = M::mock(Validator::class);
+        $tag->setValidator($validator);
+
+        $this->assertEquals($tag->getValidator(), $validator);
+    }
+
+    public function test_should_check_if_it_is_valid_when_it_is()
+    {
+        $tag = new Tag();
+        $tag->name = "Tag1";
+
+        $validator = M::mock(Validator::class);
+        $validator->shouldReceive('setRules')->with(['name'=>'required|max:255']);
+        $validator->shouldReceive('setData')->with(['name'=>'Tag1']);
+        $validator->shouldReceive('fails')->andReturn(false);
+
+        $tag->setValidator($validator);
+
+        $this->assertTrue( $tag->isValid() );
+    }
+
+    public function test_should_check_if_it_is_invalid_when_it_is()
+    {
+        $tag = new Tag();
+        $tag->name = "Tag";
+
+        $messageBag = M::mock(MessageBag::class);
+
+        $validator = M::mock(Validator::class);
+        $validator->shouldReceive('setRules')->with(['name'=>'required|max:255']);
+        $validator->shouldReceive('setData')->with(['name'=>'Tag']);
+        $validator->shouldReceive('fails')->andReturn(true);
+
+        $validator->shouldReceive('errors')->andReturn($messageBag);
+
+        $tag->setValidator($validator);
+
+        $this->assertFalse($tag->isValid());
+        $this->assertEquals($messageBag, $tag->errors);
+    }
+    
     public function test_check_if_a_category_can_be_persisted()
     {
         $tag = Tag::create(['name' => 'php','taggable_id' => 1, 'taggable_type' => 'App\User']);
@@ -23,5 +68,10 @@ class TagTest extends AbstractTestCase
 
         $tagList = Tag::all();
         $this->assertCount(1,$tagList);
+    }
+
+    public function test_if_tag_belongs_to_class_that_was_assigned()
+    {
+        
     }
 }
